39b195483682901f176c17237cf98855226fe627
trieungochai <trieungochai.dev@gmail.com>
2021-11-01 11:39:50 +0700

    Ant design icons

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
-import { AppstoreOutlined, SettingOutlined } from "@ant-design/icons";
+import {
+  AppstoreOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 
@@ -12,11 +17,24 @@ const Header = () => {
   };
 
   return (
-    <Menu onClick={handleClick} selectedKeys={current} mode="horizontal">
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Menu.Item key="home" icon={<AppstoreOutlined />}>
         Home
       </Menu.Item>
-      <SubMenu key="register" icon={<SettingOutlined />} title="Register">
+
+      <Menu.Item
+        key="register"
+        icon={<UserAddOutlined />}
+        className="float-right"
+      >
+        Register
+      </Menu.Item>
+
+      <Menu.Item key="login" icon={<UserOutlined />} className="float-right">
+        Login
+      </Menu.Item>
+
+      <SubMenu key="username" icon={<SettingOutlined />} title="Username">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
         <Menu.Item key="setting:2">Option 2</Menu.Item>
       </SubMenu>
