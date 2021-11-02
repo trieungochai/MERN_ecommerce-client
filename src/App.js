8db0bfa058820e2de55899c43f28ef9dbbe34502
trieungochai <trieungochai.dev@gmail.com>
2021-11-02 17:22:25 +0700

    Register Form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 import Header from "./components/nav/Header";
 
 const App = () => {
--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const handleSubmit = (event) => {
+    setEmail(event.target.value);
+  };
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        placeholder="johndoe@gmail.com"
+        value={email}
+        onChange={(e) => console.log(e.target.value)}
+        autoFocus
+      />
+      <button type="submit" className="btn btn-primary">
+        Register
+      </button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
+      </div>
     </div>
   );
 };
