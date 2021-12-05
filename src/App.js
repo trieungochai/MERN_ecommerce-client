e68a30629408edbfed807f408d641da707b1f9e6
trieungochai <trieungochai.dev@gmail.com>
2021-12-06 01:39:15 +0700

    User in Redux State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
 
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
@@ -10,6 +12,28 @@ import Header from "./components/nav/Header";
 import RegistrationComplete from "./pages/auth/RegistrationComplete";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+
+    // clean up
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <ToastContainer />
